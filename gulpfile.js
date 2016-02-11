ec346837433dd54f012d879c9372493f9e0141e9
fakhrishahab <fakhri1804@gmail.com>
2016-02-11 16:48:56 +0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	sass = require('gulp-sass'),
+	del = require('del');
+
+var defaultTasks = [
+	'assets',
+	'html',
+	'script',
+	'style_sass',
+	'style'
+]
+
+gulp.task('assets', function(){
+	return gulp.src('./dev/assets/**/*.*')
+	.pipe(gulp.dest('public/assets'))
+})
+
+gulp.task('html', function(){
+	return gulp.src('./dev/*.html')
+	.pipe(gulp.dest('public'))
+})
+
+gulp.task('script', function(){
+	return gulp.src('./dev/script/**/*.*')
+	.pipe(gulp.dest('public/script'))
+})
+
+gulp.task('style_sass', function(){
+	return gulp.src('./dev/style/*.scss')
+		.pipe(sass({outputStyle: 'compressed'}))
+		.pipe(sass.sync().on('Error', sass.logError))
+		.pipe(gulp.dest('public/./style'))
+})
+
+gulp.task('style', function(){
+	return gulp.src('./dev/style/*css')
+		.pipe(gulp.dest('public/style'))
+})
+
+gulp.task('del', function(cb){
+	del(defaultTasks, cb)
+})
+
+gulp.task('watch', function(){
+	gulp.watch('./dev/**/*.*', ['assets', 'html', 'script', 'style_sass','style'])
+})
+
+gulp.task('default', ['del'])
+gulp.task('zeeza_admin', defaultTasks)
+gulp.start()
